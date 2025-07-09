ffb8dd757310c4ce26c00578276acf320b64122c
Jhonnatan Machuca <jmachuca@tuxpan.com>
2025-07-09 16:58:26 -0400

    Creación del Proyecto

---
+++ src/main/java/com/jmachuca/curso/springboot/calendar/interceptor/spingboot_horario/SpingbootHorarioApplication.java
@@ -0,0 +1,13 @@
+package com.jmachuca.curso.springboot.calendar.interceptor.spingboot_horario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpingbootHorarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpingbootHorarioApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/jmachuca/curso/springboot/calendar/interceptor/spingboot_horario/controllers/AppController.java
@@ -0,0 +1,23 @@
+package com.jmachuca.curso.springboot.calendar.interceptor.spingboot_horario.controllers;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AppController {
+
+    @GetMapping("/foo")
+    public ResponseEntity<?> foo() {
+        Map<String, Object> data = new HashMap<>();
+        
+        data.put("title", "Bienvenidos al Sistema de Atención");
+        data.put("time", new Date());
+
+        return ResponseEntity.ok(data);
+    }
+}
